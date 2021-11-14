90e55a1e132fb6ab57f2234242540cf56aad6cb5
Corentin <corentin7301@gmail.com>
2021-11-14 16:09:05 +0100

    add contact for home page add home menu change menu icon png to svg

--- src/components/HomeImage.vue
+++ src/components/HomeImage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class=" relative w-1/2 cursor-pointer ml-4 mt-9 mb-16" >
-        <img @click="displayText = !displayText, ping = false" :src="`https://res.cloudinary.com/sarah3902/image/upload/f_auto/q_auto/c_scale/fl_lossy/ar_1:1,c_fill,g_north${HomeDatas.mainImage}`" alt="Sarah LEROY" class=" w-full rounded-md transition-all shadow-xl" :class="{'blurPerso':displayText}">
+  <div class=" relative w-2/3 cursor-pointer " >
+        <img @click="displayText = !displayText, ping = false" :src="`https://res.cloudinary.com/sarah3902/image/upload/q_auto/c_scale/fl_lossy/ar_1:1,c_fill,g_north${HomeDatas.mainImage}`" alt="Sarah LEROY" class=" w-full rounded-md transition-all shadow-xl" :class="{'blurPerso':displayText}">
         <!-- <div @click="displayText = !displayText" class=" absolute top-[-0.4rem] right-[-0.4rem] w-6 h-6 bg-persoBlue shadow-md rounded-full blur-sm"></div> -->
         <div v-show="ping" @click="displayText = !displayText, ping = false" class=" absolute -top-1 -right-1 w-5 h-5 bg-persoBlue rounded-full animate-ping"></div>
         <div v-show="displayText" @click="displayText = !displayText" class=" absolute top-0 left-0 flex justify-center items-center w-full h-full">
