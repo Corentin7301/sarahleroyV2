2a4a7ffece014550c1056bb197005c99b0a0cd10
Corentin <corentin7301@gmail.com>
2021-11-21 12:23:31 +0100

    contact modale

--- src/components/HomeImage.vue
+++ src/components/HomeImage.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class=" relative w-2/3 cursor-pointer " >
-        <img @click="displayText = !displayText, ping = false" :src="`https://res.cloudinary.com/sarah3902/image/upload/q_auto/c_scale/fl_lossy/ar_1:1,c_fill,g_north${HomeDatas.mainImage}`" alt="Sarah LEROY" class=" w-full rounded-md transition-all shadow-xl" :class="{'blurPerso':displayText}">
-        <!-- <div @click="displayText = !displayText" class=" absolute top-[-0.4rem] right-[-0.4rem] w-6 h-6 bg-persoBlue shadow-md rounded-full blur-sm"></div> -->
-        <div v-show="ping" @click="displayText = !displayText, ping = false" class=" absolute -top-1 -right-1 w-5 h-5 bg-persoBlue rounded-full animate-ping"></div>
-        <div v-show="displayText" @click="displayText = !displayText" class=" absolute top-0 left-0 flex justify-center items-center w-full h-full">
-          <h2 class=" text-lightColor text-center text-sm p-2 transition-all">{{HomeDatas.description}}</h2>
+  <div class=" relative w-1/2 cursor-pointer " >
+        <!-- img mobile -->
+        <img @click="displayText = !displayText, ping = false" :src="`https://res.cloudinary.com/sarah3902/image/upload/q_auto/fl_lossy/ar_1:1,c_fill,g_north${HomeDatas.mainImage}`" alt="Sarah LEROY" class=" lg:hidden w-full rounded-md transition-all shadow-xl" :class="{'blurPerso':displayText}">
+        <!-- img desktop -->
+        <img :src="`https://res.cloudinary.com/sarah3902/image/upload/q_auto/fl_lossy/ar_5:3,c_fill,g_face${HomeDatas.mainImage}`" alt="Sarah LEROY" class=" hidden lg:block w-full rounded-md transition-all shadow-xl cursor-auto">
+        <div v-show="ping" @click="displayText = !displayText, ping = false" class="lg:hidden absolute -top-1 -right-1 w-5 h-5 bg-persoBlue rounded-full animate-ping"></div>
+        <div v-show="displayText" @click="displayText = !displayText" class=" lg:hidden absolute top-0 left-0 flex justify-center items-center w-full h-full">
+          <h2 class=" text-lightColor text-[12px] p-2 transition-all">{{HomeDatas.description}}</h2>
         </div>
+
+        <h2 class=" hidden lg:block text-lightColor text-center text-sm p-2 transition-all">{{HomeDatas.description}}</h2>
   </div>
 </template>
 
