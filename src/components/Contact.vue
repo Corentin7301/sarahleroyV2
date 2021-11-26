327435f5b61ce1e3e25d669029e795779d18713d
Corentin <corentin7301@gmail.com>
2021-11-26 09:51:17 +0100

    fix small design

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="isOpen = !isOpen"><svg class="w-14 h-14 text-lightColor" fill="none" stroke="currentColor"
+    <button @click="isOpen = !isOpen"><svg class="w-12 h-12 text-lightColor" fill="none" stroke="currentColor"
         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
           d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
