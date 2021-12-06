3fbd2a59831a86912a573a8e5e057f356d8fc626
Corentin <corentin7301@gmail.com>
2021-12-06 19:18:21 +0100

    small design fix

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="isOpen = !isOpen"><svg class="w-12 h-12 text-lightColor" fill="none" stroke="currentColor"
+    <button @click="isOpen = !isOpen"><svg class="w-12 h-12 text-lightColor hover:opacity-75 transition-all" fill="none" stroke="currentColor"
         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
           d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
@@ -10,7 +10,7 @@
 
     <!-- modale -->
 
-    <div v-show="isOpen" class="fixed z-30 blurPerso top-0 left-0 w-screen h-screen overflow-y-auto overflow-x-hidden">
+    <div v-show="isOpen" class="fixed z-30 blurPerso top-0 left-0 w-screen h-screen ">
     </div>
     <div v-show="isOpen"
       class=" bg-persoBlue w-[85%] lg:w-1/3 z-50 absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 rounded-md py-5 px-6 transition-all">
@@ -21,9 +21,9 @@
       </svg>
       <p class=" text-lightColor text-center lg:text-2xl">Contact</p>
       <form action="/thanks" method="POST" name="contact-portfolio" netlify netlify-honeypot="bot-field" class=" space-y-3 mt-5 flex flex-col justify-center items-center">
-        <input type="text" name="name" id="name" placeholder="Nom et prénom">
-        <input type="email" name="email" id="email" placeholder="Email">
-        <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+        <input type="text" name="name" id="name" placeholder="Nom et prénom" required>
+        <input type="email" name="email" id="email" placeholder="Email" required>
+        <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" required></textarea>
         <input type="submit" value="Envoyer" class=" bg-white">
       </form>
       <div class="space-y-2 mt-2">
@@ -82,11 +82,11 @@
   }
 
   input[type=submit] {
-    @apply w-32 m-0 self-end;
+    @apply w-32 m-0 self-end border-2 border-transparent transition-all cursor-pointer;
     color: rgba(0, 0, 0, 0.45);
 
     &:hover {
-      @apply text-lightColor bg-persoBlue border-2 border-lightColor box-border transition-all;
+      @apply text-lightColor bg-persoBlue border-lightColor box-border transition-all;
     }
   }
 </style>
